cab7a690d4eb5119cfec87e09df8efc09e47fd80
Martin263-gif <marijaagejevaite@gmail.com>
2024-12-03 19:22:54 +0200

    2024-12-03

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,11 +7,21 @@ public class Player : MonoBehaviour
     [Header("Movement")]
     public float movementSpeed = 10;
     public float jumpHeight = 3;
+    public float dashSpeed = 20;
+    public float doubleJumpHeight = 6;
 
     [Header("Jump")]
     public Transform groundCheck; // player legs
     public LayerMask groundLayer;
     public float groundCheckRadius = 0.2f;
+
+    [Header("Jump Machanics")]
+    public float coyoteTime = 0.3f;
+    public float jumpBufferTime = 0.3f;
+
+
+    private float jumpBufferCounter;
+    private float coyoteTimeCounter;
 
 
     private bool isGrounded;
@@ -29,8 +39,28 @@ public class Player : MonoBehaviour
         inputX = Input.GetAxisRaw("Horizontal");
 
         isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
-        if(isGrounded && Input.GetButtonDown("Jump"))
+
+        if (isGrounded)
         {
+            coyoteTimeCounter = coyoteTime;
+        }
+        else
+        {
+            coyoteTimeCounter -= Time.deltaTime;
+        }
+
+        if(Input.GetButtonDown("Jump"))
+        {
+            jumpBufferCounter = jumpBufferTime;
+        }
+        else
+        {
+            jumpBufferCounter -= Time.deltaTime;
+        }
+
+        if(coyoteTimeCounter > 0 && jumpBufferCounter > 0)
+        {
+            jumpBufferCounter = 0; //prevent infinite jump
             var jumpVelocity = Mathf.Sqrt(jumpHeight * -2f *
                 Physics.gravity.y * rb.gravityScale);
             rb.velocity = new Vector2(rb.velocity.x, jumpVelocity);
